862c863b4be53a4b4f037178775da9956205e09e
Revolt77777 <115503796+Revolt77777@users.noreply.github.com>
2023-10-09 20:16:06 -0400

    Update playercontroller

--- Legend of Medal/Assets/Scripts/Player/PlayerMovement.cs
+++ Legend of Medal/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,19 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 15f;
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal"); // Using GetAxisRaw for direct input
+        float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, 0.0f, vertical) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical).normalized;
+
+        // Rotate the movement direction by 45 degrees around the y-axis
+        movement = Quaternion.Euler(0, 45, 0) * movement;
+
+        movement = movement * speed * Time.deltaTime;
 
         transform.Translate(movement);
     }
 }
-
